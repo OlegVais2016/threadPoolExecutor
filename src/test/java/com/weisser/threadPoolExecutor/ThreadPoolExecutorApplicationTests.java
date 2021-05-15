871450e095d247bf0d93fbc6f195b2d22d1d0a6f
Oleg Vaiser <aly9681082@gmail.com>
2021-05-15 17:54:44 +0300

    first

---
+++ src/test/java/com/weisser/threadPoolExecutor/ThreadPoolExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weisser.threadPoolExecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadPoolExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
